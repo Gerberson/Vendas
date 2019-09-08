098cc4cd249e4c9e952aeeb3e55d621080c38a60
Gerberson Dias <gerbersondias.312@gmail.com>
2019-09-08 19:17:47 -0300

    Add busca simples

---
+++ Vendas/Controllers/SalesRecordsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vendas.Services;
+
+namespace Vendas.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewBag.minDate = minDate.Value.ToString("yyyy-MM-dd");
+            ViewBag.maxDate = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
